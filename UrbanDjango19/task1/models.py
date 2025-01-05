bf675dc848943080d00f3e949681cfc64599f227
HuntWiz <botalov.gleb123@gmail.com>
2025-01-05 17:55:34 +0500

    4

--- UrbanDjango19/task1/models.py
+++ UrbanDjango19/task1/models.py
@@ -3,9 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Buyer(models.Model):
-    name = models.CharField(name='username', max_length=100)
-    balance = models.DecimalField(max_digits=100, decimal_places=3)
-    age = models.IntegerField()
+    name = models.CharField(name='username', max_length=100, verbose_name='Логин пользователя')
+    balance = models.DecimalField(max_digits=100, decimal_places=3, default=100, verbose_name='Баланс')
+    age = models.IntegerField(verbose_name='Возраст')
+
 
     class Meta:
         verbose_name = 'Покупатели'
@@ -13,12 +14,12 @@ class Buyer(models.Model):
 
 
 class Game(models.Model):
-    title = models.CharField(max_length=100)
-    cost = models.DecimalField(max_digits=20, decimal_places=3)
+    title = models.CharField(max_length=100, verbose_name='Название игры')
+    cost = models.DecimalField(max_digits=20, decimal_places=3, verbose_name='Цена')
     size = models.DecimalField(max_digits=30, decimal_places=3)
     description = models.TextField(blank=True)
     age_limited = models.BooleanField(default=False)
-    buyer = models.ManyToManyField(Buyer, related_name='games')
+    buyer = models.ManyToManyField(Buyer, related_name='games', related_query_name='game')
 
     class Meta:
         verbose_name = 'Игры'
